300fa8a36562839ad535bde95e4bdba7ded234b5
purdialex <purdialex67@gmail.com>
2025-01-10 23:44:53 +0200

    Added some stuff to the scene, have to make crates drop items

--- scenes/container/item_container.gd
+++ scenes/container/item_container.gd
@@ -1,2 +1,11 @@
 extends StaticBody2D
 class_name ItemContainer
+
+
+signal open(pos, direction)
+#Before ready function
+@onready var current_direction: Vector2 = Vector2.DOWN.rotated(rotation)
+
+# ready:
+# applying rotation
+#creating the node
